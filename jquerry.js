8b1675cd115499e772e2fbfdb9416b7bfd5a4f6c
Zolejtanos <74880401+Zolejtanos@users.noreply.github.com>
2021-10-15 22:35:30 +0200

    Add files via upload

---
+++ jquerry.js
@@ -0,0 +1,7 @@
+
+$(document).ready(function () {
+    $('#myModal').on('show.bs.modal', function (e) {
+        var image = $(e.relatedTarget).attr('src');
+        $(".img-responsive").attr("src", image);
+    });
+});
